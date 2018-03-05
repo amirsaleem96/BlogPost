7c5514dbecfcffd29fdbb09cdecb63745d253611
= <amirsaleem96@gmail.com>
2018-03-05 22:58:48 +0530

    Add integer method

--- src/commons/common.js
+++ src/commons/common.js
@@ -1,11 +1,16 @@
-import axios from 'axios';
-
-var asyncQuery = () => {
-    return new Promise((resolve, reject) => {
-        axios.get('/test/api').then(resolve).catch(reject);        
-    });
+const getInteger = (variable) => {
+    if(typeof variable != 'number'){
+        try {
+            if(isNaN(parseInt(variable))) {
+                return 0;
+            } else {
+                return parseInt(variable);
+            }
+        } catch (e) {
+            return 0;
+        }
+    } else {
+        return variable
+    }
+    return variable;
 }
-
-async().then((data) => {
-    console.log(data);
-})
