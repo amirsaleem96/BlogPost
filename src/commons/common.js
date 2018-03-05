320cd7999b4565b9e275e4857e1a2e04149d4334
= <amirsaleem96@gmail.com>
2018-03-05 23:06:18 +0530

    Add checks and comments

--- src/commons/common.js
+++ src/commons/common.js
@@ -1,16 +1,21 @@
-const getInteger = (variable) => {
+/**
+ * @param {integer or string} variable
+ * @return returns the variable as passed as an integer value, defaults to 0 
+ */
+export const getInteger = (variable = 0) => {
+    // check if argument is not of type number
     if(typeof variable != 'number'){
         try {
+            // check if argument after converting to an integer is not a number 
             if(isNaN(parseInt(variable))) {
                 return 0;
-            } else {
-                return parseInt(variable);
             }
+            return parseInt(variable);
         } catch (e) {
             return 0;
         }
     } else {
-        return variable
+        return variable;
     }
     return variable;
 }
