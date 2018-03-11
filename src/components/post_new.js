ebbca5a82431a4ee6e5088be51120b1e5ecbea77
= <amirsaleem96@gmail.com>
2018-03-11 23:10:32 +0530

    Add redux-form state and error

--- src/components/post_new.js
+++ src/components/post_new.js
@@ -5,27 +5,60 @@ import { Field, reduxForm } from 'redux-form';
 class PostNew extends Component {
 
     renderField ( field ) {
+        const { meta: { touched, error } } = field;
+        const className = `form-group ${touched && error ? 'has-danger' : ''}`;
         return(
-            <div className = 'form-group'>
+            <div className = {className}>
                 <label>{ field.label }</label>
                 <input
                     className = 'form-control'
                     type = "text"
                     {...field.input}
                 />
+                <div className = 'text-help'>{ touched ? error : '' }</div>
             </div>
         )
     }
 
+    onSubmit( values ) {
+
+    }
+
     render() {
+
+        const { handleSubmit } = this.props;
+
         return(
-            <form>
+            <form onSubmit = { handleSubmit(this.onSubmit.bind(this)) }>
                 <Field label = 'Title' name = 'title' component = { this.renderField } />
-                <Field label = 'Catgeories' name = 'tags' component = { this.renderField } />
+                <Field label = 'Catgeories' name = 'categories' component = { this.renderField } />
                 <Field label = 'Content' name = 'content' component = { this.renderField } />
+                <button type = 'submit' className = 'btn btn-primary'>Save</button>
             </form>
         )
     }
 }
 
-export default reduxForm({ form: 'PostsNewForm' })(PostNew);
+function validate ( values ) {
+
+    const errors = {};
+
+    if(!values.title) {
+        errors.title = 'Enter a title!';
+    }
+
+    if(!values.categories) {
+        errors.categories = 'Enter some categories';
+    }
+
+    if(!values.content) {
+        errors.content = 'Enter some content please';
+    }
+
+    return errors;
+}
+
+export default reduxForm({
+    validate, 
+    form: 'PostsNewForm' 
+})(PostNew);
